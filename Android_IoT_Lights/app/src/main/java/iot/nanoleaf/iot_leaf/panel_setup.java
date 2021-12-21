9c7df89d927e6cfb2c042a2d25c240b1fb30e8cf
JNardoni <71414044+JNardoni@users.noreply.github.com>
2021-12-20 21:40:03 -0600

    Images
    Added more images

--- Android_IoT_Lights/app/src/main/java/iot/nanoleaf/iot_leaf/panel_setup.java
+++ Android_IoT_Lights/app/src/main/java/iot/nanoleaf/iot_leaf/panel_setup.java
@@ -1,7 +1,29 @@
 package iot.nanoleaf.iot_leaf;
 
-public class panel_setup {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
+public class panel_setup extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.panel_layout);
 
 
+        findViewById(R.id.triangle1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    public void onClick() {
+
+
+    }
 
 }
