19c9a43720194821f3c949f44974852a52837b37
JNardoni <71414044+JNardoni@users.noreply.github.com>
2022-01-27 01:55:53 -0600

    Modes
    Added color functionality for modes

--- Android_IoT_Lights/app/src/main/java/iot/nanoleaf/iot_leaf/AddMode.java
+++ Android_IoT_Lights/app/src/main/java/iot/nanoleaf/iot_leaf/AddMode.java
@@ -2,10 +2,18 @@ package iot.nanoleaf.iot_leaf;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
 
 import java.math.RoundingMode;
 
@@ -13,7 +21,13 @@ public class AddMode extends Activity {
 
     SeekBar SeekSpeed;
     TextView TextSpeed;
+    ImageView Pattern1;
+    ImageView Pattern2;
 
+    int selected = 1;
+
+    private View root;
+    int currentBackgroundColor = 0xffffffff;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +53,61 @@ public class AddMode extends Activity {
             }
         });
 
+        Pattern1 = (ImageView) findViewById(R.id.imgPattern1);
+        Pattern2 = (ImageView) findViewById(R.id.imgPattern2);
+
+        Pattern1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selected = 1;
+                Pattern1.setPadding(10,10,10,10);
+                Pattern2.setPadding(0,0,0,0);
+            }
+        });
+        Pattern2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selected = 2;
+                Pattern2.setPadding(10,10,10,10);
+                Pattern1.setPadding(0,0,0,0);
+
+                final Context context = AddMode.this;
+
+                ColorPickerDialogBuilder
+                        .with(context)
+                        .setTitle("Choose color")
+                        .initialColor(currentBackgroundColor)
+                        .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                        .density(12)
+                        .setOnColorSelectedListener(new OnColorSelectedListener() {
+                            @Override
+                            public void onColorSelected(int selectedColor) {
+                                Toast.makeText(context,"onColorSelected: 0x" + Integer.toHexString(selectedColor),Toast.LENGTH_SHORT).show();
+                                //Toast.makeText(context, "Max modes set!", Toast.LENGTH_SHORT).show();;
+                            }
+                        })
+                        .setPositiveButton("ok", new ColorPickerClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                                changeBackgroundColor(selectedColor);
+                            }
+                        })
+                        .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                        .build()
+                        .show();
+            }
+        });
+
+
+    }
+
+    private void changeBackgroundColor(int selectedColor) {
+        currentBackgroundColor = selectedColor;
+        root.setBackgroundColor(selectedColor);
     }
 
 }
